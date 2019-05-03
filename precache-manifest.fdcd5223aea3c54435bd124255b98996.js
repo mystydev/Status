38d586d76f3026200c8accd385510a6f05d9e98f
unknown <j.ed.glossop@gmail.com>
2019-05-04 00:29:24 +0100

    Updates

---
+++ precache-manifest.fdcd5223aea3c54435bd124255b98996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75a2c11aa6ed469747133a4787aeb56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87df7f1ad90d10043743",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "1dd973a14c050a62a2b8",
+    "url": "/static/js/2.a6a52406.chunk.js"
+  },
+  {
+    "revision": "87df7f1ad90d10043743",
+    "url": "/static/js/main.aa64b8c6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
